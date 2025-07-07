afd626098ec4f002a8f3cdb291cc4d2fdc01f089
Кравцов Иван <ivan.an.kravtsov@gazprombank.ru>
2025-07-07 14:57:26 +0300

    Dz 2

--- main.go
+++ main.go
@@ -11,4 +11,18 @@ func main() {
 	const EURtoRUB = USDinRUB / USDinEUR
 
 	fmt.Println(EURtoRUB)
+
+	number := getNumber()
+
+	fmt.Println(number)
+}
+
+func getNumber() int {
+	var n int
+	fmt.Scan(&n)
+	return n
+}
+
+func calculateCurrency(x int, currency1 string, currency2 string) {
+
 }
